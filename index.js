a3a91b0624d47d88f366a294a5010919dbedd2a0
Rakib Gazi <gazicourses@gmail.com>
2025-02-04 23:45:12 +0600

    user delete api is added

--- index.js
+++ index.js
@@ -153,6 +153,13 @@ async function run() {
       });
     });
 
+    app.delete("/applicant-delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mohanobirShikkhaCollection.deleteOne(query);
+      res.json(result);
+    });
+
     // mohanobir shikkha end here
     // User Api
     app.get("/users", verifyToken, async (req, res) => {
